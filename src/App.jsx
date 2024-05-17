7e93675cb67740fc40869c3d46fec369e60b2b84
Samantha <samantha@Samanthas-MBP.lan>
2024-05-17 05:21:34 -0700

    update home and app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Chat from "./pages/Chat";
 
 const App = () => {
     return (
     <div className="App">
         <Route path="/" component={Home} />
-        <Route path="/chats" />
+        <Route path="/chats" component={Chat} />
         </div>
     );
   }
