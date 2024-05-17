fdea7da123276152ec8173322e0a91204e375bfa
Samantha <samantha@Samanthas-MBP.lan>
2024-05-17 03:35:30 -0700

    update Chat.jsx

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import axios from 'axios';
 
 const Chat = () => {
+const [chats, setChats] = useState([]);
+
     const fetchChats = async() => {
         const data = await axios.get('/api/chat')
-        console.log(data);
+        setChats(data);
     };
 
     useEffect(() => {
         fetchChats();
     }, []);
-    
-    return <div>Chats</div>;
+
+    return (
+    <div>
+        {chats.map((chat) => (
+        <div key>{chat._id} {chat.chatName}</div>
+        ))}
+        </div>
+    );
 };
 
 export default Chat;
