d652999001e0b446f77c0d3999abd9cbec4ea7bf
Samantha <samantha@Samanthas-MBP.lan>
2024-05-20 10:44:16 -0700

    update avatar user badge component

--- src/components/userAvatar/UserBadgeItem.js
+++ src/components/userAvatar/UserBadgeItem.js
@@ -1,4 +1,25 @@
 import { Badge } from "@chakra-ui/layout";
 import { CloseIcon } from "@chakra-ui/icons";
 
+const UserBadgeItem = ({ user, handleFunction, admin }) => {
+    return (
+      <Badge
+        px={2}
+        py={1}
+        borderRadius="md"
+        m={1}
+        mb={2}
+        fontSize={10}
+        variant="solid"
+        cursor="pointer"
+        colorScheme="blue"
+        onClick={handleFunction}
+      >
+        {user.name}
+        {admin === user._id && <span> (Admin)</span>}
+        <CloseIcon pl={1} />
+      </Badge>
+    );
+  };
+
 export default UserBadgeItem;
