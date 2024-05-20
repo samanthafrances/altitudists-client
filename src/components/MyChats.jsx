ec8f44ebeecaea4b8d33e81d0da9e824310439da
Samantha <samantha@Samanthas-MBP.lan>
2024-05-20 08:19:37 -0700

    update my chats component

--- src/components/MyChats.jsx
+++ src/components/MyChats.jsx
@@ -26,8 +26,8 @@ const MyChats = ({ fetchAgain }) => {
         setChats(data);
       } catch (error) {
         toast({
-          title: "Error Occured!",
-          description: "Failed to Load the chats",
+          title: "Error",
+          description: "Chats could not load",
           status: "error",
           duration: 5000,
           isClosable: true,
@@ -35,6 +35,89 @@ const MyChats = ({ fetchAgain }) => {
         });
       }
     };
+
+    useEffect(() => {
+        setLoggedUser(JSON.parse(localStorage.getItem("userInfo")));
+        fetchChats();
+      }, [fetchAgain]);
+    
+      return (
+        <Box
+          d={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+          flexDir="column"
+          alignItems="center"
+          p={3}
+          bg="white"
+          w={{ base: "100%", md: "30%" }}
+          borderRadius="lg"
+          borderWidth="1px"
+        >
+          <Box
+            pb={3}
+            px={3}
+            fontSize={{ base: "30px", md: "25px" }}
+            fontFamily=""
+            d="flex"
+            w="100%"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            My Chats
+            <GroupChatModal>
+              <Button
+                d="flex"
+                fontSize={{ base: "15px", md: "10px", lg: "15px" }}
+                rightIcon={<AddIcon />}
+              >
+                New Group Chat
+              </Button>
+            </GroupChatModal>
+          </Box>
+          <Box
+            d="flex"
+            flexDir="column"
+            p={3}
+            bg="#ffffff"
+            w="100%"
+            h="100%"
+            borderRadius="md"
+            overflowY="hidden"
+          >
+            {chats ? (
+              <Stack overflowY="scroll">
+                {chats.map((chat) => (
+                  <Box
+                    onClick={() => setSelectedChat(chat)}
+                    cursor="pointer"
+                    bg={selectedChat === chat ? "#ffffff" : "#ffffff"}
+                    color={selectedChat === chat ? "white" : "black"}
+                    px={3}
+                    py={2}
+                    borderRadius="lg"
+                    key={chat._id}
+                  >
+                    <Text>
+                      {!chat.isGroupChat
+                        ? getSender(loggedUser, chat.users)
+                        : chat.chatName}
+                    </Text>
+                    {chat.latestMessage && (
+                      <Text fontSize="xs">
+                        <b>{chat.latestMessage.sender.name} : </b>
+                        {chat.latestMessage.content.length > 50
+                          ? chat.latestMessage.content.substring(0, 51) + "..."
+                          : chat.latestMessage.content}
+                      </Text>
+                    )}
+                  </Box>
+                ))}
+              </Stack>
+            ) : (
+              <ChatLoading />
+            )}
+          </Box>
+        </Box>
+      );
 };
 
 export default MyChats;
