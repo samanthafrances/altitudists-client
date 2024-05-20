e34509e4a7b495fefa5fafaeeed0c677ecde9494
Samantha <samantha@Samanthas-MBP.lan>
2024-05-20 10:15:50 -0700

    update avatar components

---
+++ src/components/userAvatar/UserBadgeItem.js
@@ -0,0 +1,4 @@
+import { Badge } from "@chakra-ui/layout";
+import { CloseIcon } from "@chakra-ui/icons";
+
+export default UserBadgeItem;
---
+++ src/components/userAvatar/UserListItem.js
@@ -0,0 +1,5 @@
+import { Box, Text } from "@chakra-ui/layout";
+import { ChatState } from "../../Context/ChatProvider";
+import { Avatar } from "@chakra-ui/avatar";
+
+export default UserListItem;
