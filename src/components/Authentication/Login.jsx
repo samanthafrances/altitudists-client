b43f9cf06ba1e7ac4f78d61a345010b94ae6e569
Samantha <samantha@Samanthas-MBP.lan>
2024-05-19 23:20:44 -0700

    update login component

--- src/components/Authentication/Login.jsx
+++ src/components/Authentication/Login.jsx
@@ -1,13 +1,72 @@
 import React from 'react'
+import { useState } from "react";
+import axios from "axios";
+import { ChatState } from "../../Context/ChatProvider";
+import { VStack } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
+import { useToast } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
 
 const Login = () => {
     const [show, setShow] = useState(false);
-    const [name, setName] = useState("");
+    const handleClick = () => setShow(!show);
+    const toast = useToast();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState("");
+
+    const history = useHistory();
+    const { setUser } = ChatState();
     
-    const handleClick = () => setShow(!show);
-    const submitHandler = () => {};
+    const submitHandler = async () => {
+        setLoading(true);
+        if (!email || !password) {
+          toast({
+            title: "Please Fill all the Feilds",
+            status: "warning",
+            duration: 5000,
+            isClosable: true,
+            position: "bottom",
+          });
+          setLoading(false);
+          return;
+    }
+    try {
+        const config = {
+          headers: {
+            "Content-type": "application/json",
+          },
+        };
+  
+        const { data } = await axios.post(
+          "/api/user/login",
+          { email, password },
+          config
+        );
+        toast({
+          title: "Login Successful",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom",
+        });
+        setUser(data);
+        localStorage.setItem("userInfo", JSON.stringify(data));
+        setLoading(false);
+        history.push("/chats");
+      } catch (error) {
+        toast({
+          title: "Error Occured!",
+          description: error.response.data.message,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom",
+        });
+        setLoading(false);
+      }
+};
 
   return (
 
