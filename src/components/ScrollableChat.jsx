e895ff7a0809a757dfd82f3b4d21df887889aaa6
Samantha <samantha@Samanthas-MBP.lan>
2024-05-20 09:42:34 -0700

    update scrollable chat component

---
+++ src/components/ScrollableChat.jsx
@@ -0,0 +1,12 @@
+import { ChatState } from "../Context/ChatProvider";
+import ScrollableFeed from "react-scrollable-feed";
+import { Avatar } from "@chakra-ui/avatar";
+
+
+const ScrollableChat = ({ messages }) => {
+    const { user } = ChatState();
+
+    
+}
+
+export default ScrollableChat;
