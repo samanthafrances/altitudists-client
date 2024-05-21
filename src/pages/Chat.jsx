cb05fbe1b9c5af05f19c20fb04f070c59f98dd1b
Samantha <samantha@Samanthas-MBP.lan>
2024-05-21 11:49:12 -0700

    debug

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from "react";
 import { Box } from "@chakra-ui/layout";
-import Chatbox from "../components/Chatbox";
+import Chatbox from "../components/ChatBox";
 import MyChats from "../components/MyChats";
 import SideDrawer from "../components/miscellaneous/SideDrawer";
 import { ChatState } from "../Context/ChatProvider";
