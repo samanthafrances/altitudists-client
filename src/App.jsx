2f359029e699d2a310b44caaf3685dd060f1dc29
Samantha <samantha@Samanthas-MBP.lan>
2024-05-21 08:16:30 -0700

    update socket.js and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { ChakraProvider } from "@chakra-ui/react";
-import { Socket } from "socket.io-client";
 import Home from "./pages/Home";
 import Nav from "./components/Nav";
 
