357adfee3b15d7d27ad5d38f86bb32e35d2e00df
Samantha <samantha@Samanthas-MBP.lan>
2024-05-22 00:17:46 -0700

    debug render error

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "./App.css";
 import Home from "./pages/Home";
+import Chat from "./pages/Chat";
 
 function App() {
   return (
     <Router>
-      <Home />
+      <div className="App">
+        <Route path="/" component={Home} exact />
+        <Route path="/chats" component={Chat} />
+      </div>
     </Router>
   );
 }
+
 
 export default App;
