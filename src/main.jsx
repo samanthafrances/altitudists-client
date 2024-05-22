bde8b9028d59ef14fc2207d545a33bb60633dc76
Samantha <samantha@Samanthas-MBP.lan>
2024-05-21 22:28:43 -0700

    debug render

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
 import { io } from "socket.io-client";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import Index from "./pages/index.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
-ReactDOM.render(
+const socket = io("http://localhost:3001");
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider>
-        <App socket={socket} />
+        <Index socket={socket} />
       </ChakraProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
