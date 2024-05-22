47da1d12b21af76535a7041ceff24cbc9a473fe4
Samantha <samantha@Samanthas-MBP.lan>
2024-05-21 22:35:06 -0700

    debug render

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
-const socket = io("http://localhost:3001");
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
