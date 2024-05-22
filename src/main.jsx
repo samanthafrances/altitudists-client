be493f5af69bf89685d8393376e04859742e840c
Samantha <samantha@Samanthas-MBP.lan>
2024-05-21 23:13:26 -0700

    debug render error

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import IndexPage from "./pages/IndexPage";
+import Index from "./components/Index";
+
+const socket = io("http://localhost:5000");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider>
-        <Index socket={socket} />
+        <IndexPage socket={socket} /> {/* render the IndexPage component */}
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
